61e0f5ef76226e59e6e13e8d6fb6fe25e5455f58
Sivaraman12345 <sivaraman.m12345@gmail.com>
2025-03-17 19:07:26 +0530

    Change

--- longestSubstring.py
+++ longestSubstring.py
@@ -13,17 +13,3 @@ while True:
     print(maxlen)
 # Time Complexity: O(n)
 # Space Complexity: O(n)
-# Input: "abcabcbb"
-# Output: 3
-# Input: "bbbbb"
-# Output: 1
-# Input: "pwwkew"
-# Output: 3
-# Input: ""
-# Output: 0
-# Input: " "
-# Output: 1
-# Input: "au"
-# Output: 2
-# Input: "dvdf"
-# Output: 3
